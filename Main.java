1ce5f1a00b8df9d999fa2a922d3303f0b4cceb13
JavvaCode <122830055+JavvaCode@users.noreply.github.com>
2023-01-16 21:10:04 +0100

    Calc commit
    Easy calculation

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 77;
+        int b = 123;
+
+        int c = a + b;
+        System.out.println("Сложение а и б составляет = " + c);
+
+    }
+
+}
